877176dfd0993fecbdb05b60937b8d94a36110a7
Alexander0317 <56885275+Alexander0317@users.noreply.github.com>
2019-11-14 19:50:45 -0500

    Update login.py

--- login.py
+++ login.py
@@ -84,7 +84,7 @@ def validar():
         messagebox.showwarning("Cuidado","Dato incorrecto")
 
 #Validaciones del registro
-def control():
+def Controlar():
     cadena=Nombres.get()
     patron="([A-Za-z])"
     cadena1=Correo.get()
@@ -94,12 +94,11 @@ def control():
     cadena3=Identificaion.get()
     patron3="\d{10}$"
     cadena4=Apellidos.get()
-    patron4="([A-Za-z])"
     if(re.match(patron,cadena)):
         if(re.match(patron1,cadena1)):
             if(re.match(patron2,cadena2)):
                 if(re.match(patron3,cadena3)):
-                    if(re.match(patron4,cadena4)):
+                    if(re.match(patron,cadena4)):
                         print("formulario de registro correcto")
                         database()
                     else:
@@ -114,16 +113,16 @@ def control():
         messagebox.showwarning("Cuidado","Nombre no valido")
 
 def limpiar():
-    fecha_produccion = Fecha_produccion.set("")
-    identificaion=Identificaion.set("")
-    mazorcas = Mazorcas.set("")
-    mazorcasnocomercial=Mazorcasnocomercial.set("")
-    mazorcascomercial=Mazorcascomercial.set("")
-    nombres=Nombres.set("")
-    apellidos=Apellidos.set("")
-    edad=Edad.set("")
-    correo=Correo.set("")
-    password=Password.set("")
+    Fecha_produccion.set("")
+    Identificaion.set("")
+    Mazorcas.set("")
+    Mazorcasnocomercial.set("")
+    Mazorcascomercial.set("")
+    Nombres.set("")
+    Apellidos.set("")
+    Edad.set("")
+    Correo.set("")
+    Password.set("")
 
 def database():
     fecha_produccion = Fecha_produccion.get()
@@ -145,33 +144,33 @@ def registrarusu():
     new.configure(background='sea green')
     new.title("Registrar Usuario")
     
-    esw=tk.Label(new,text="Digite el siguiente formulario" ,bg="lawn green").pack(padx=5,pady=5,ipadx=5,ipady=5)
+    Label(new,text="Digite el siguiente formulario" ,bg="lawn green").pack(padx=5,pady=5,ipadx=5,ipady=5)
 
-    label_1=tk.Label(new,text="Identificacion: ", font=("Agency FB",14)).place(x=10,y=45)
+    Label(new,text="Identificacion: ", font=("Agency FB",14)).place(x=10,y=45)
     entry_1 = tk.Entry(new,textvar=Identificaion)
     entry_1.pack(padx=5,pady=5,ipadx=5,ipady=5)
 
-    label_11=tk.Label(new,text="Nombre: ", font=("Agency FB",14)).place(x=10,y=85)
+    Label(new,text="Nombre: ", font=("Agency FB",14)).place(x=10,y=85)
     entry_11 = tk.Entry(new,textvar=Nombres)
     entry_11.pack(padx=5,pady=5,ipadx=5,ipady=5)
 
-    label_4=tk.Label(new,text="Apellido: ", font=("Agency FB",14)).place(x=10,y=125)
+    Label(new,text="Apellido: ", font=("Agency FB",14)).place(x=10,y=125)
     entry_4 = tk.Entry(new,textvar=Apellidos)
     entry_4.pack(padx=5,pady=5,ipadx=5,ipady=5)
 
-    label_5=tk.Label(new,text="Edad: ", font=("Agency FB",14)).place(x=10,y=165)
+    Label(new,text="Edad: ", font=("Agency FB",14)).place(x=10,y=165)
     entry_5= tk.Entry(new,textvar=Edad)
     entry_5.pack(padx=5,pady=5,ipadx=5,ipady=5)
 
-    label_2=tk.Label(new,text="Correo: ", font=("Agency FB",14)).place(x=10,y=205)
+    Label(new,text="Correo: ", font=("Agency FB",14)).place(x=10,y=205)
     entry_2= tk.Entry(new,textvar=Correo)
     entry_2.pack(padx=5,pady=5,ipadx=5,ipady=5)
 
-    label_6=tk.Label(new,text="Password: ", font=("Agency FB",14)).place(x=10,y=245)
+    Label(new,text="Password: ", font=("Agency FB",14)).place(x=10,y=245)
     entry_6= tk.Entry(new,textvar=Password, show="*")
     entry_6.pack(padx=5,pady=5,ipadx=5,ipady=5)
 
-    label_3=tk.Label(new,text="Rol: ", font=("Agency FB",14)).place(x=10,y=295)
+    Label(new,text="Rol: ", font=("Agency FB",14)).place(x=10,y=295)
 
     Radiobutton(new, text="User",padx = 5, variable=Tipo, value="user").pack(padx=4,pady=4,ipadx=4,ipady=4)
     Radiobutton(new, text="Admin",padx = 20, variable=Tipo, value="admin").pack(padx=4,pady=4,ipadx=4,ipady=4)
@@ -184,60 +183,15 @@ def registrarusu():
     Estado.set(value=1) 
     droplist.pack(padx=4,pady=4,ipadx=4,ipady=4)
 
-    Button(new,text="Registrar",bg='brown', font=("black",12),command=control).place(x=10,y=445)
+    Button(new,text="Registrar",bg='brown', font=("black",12),command=Controlador).place(x=10,y=445)
     Button(new,text="Buscar",bg='brown', font=("black",12),command=traer).place(x=92,y=445)
     Button(new,text="Actualizar",bg='brown', font=("black",12),command=consulta).place(x=160,y=445)
     Button(new,text="Borrar",bg='brown', font=("black",12),command=limpiar).place(x=245,y=445)
 
-    boton20 = tk.Button(new,text="Cerrar",bg='orange', font=("black",12),command=new.destroy).place(x=100,y=490)
+    Button(new,text="Cerrar",bg='orange', font=("black",12),command=new.destroy).place(x=100,y=490)
 #-----------------ventana-de-produccion------------------------------
 
-def registrarProduccion():
-    
-    ventana.deiconify()
-    new = tk.Toplevel()
-    new.geometry("500x500+700+5")
-    new.configure(background='sea green')
-    new.title("Registrar Produccion")
-    esw=tk.Label(new,text="Digite el siguiente formulario" ,bg="lawn green").pack(padx=5,pady=5,ipadx=5,ipady=5)
-
-    label_17=tk.Label(new,text="Fecha de produccion: ", font=("Agency FB",14)).place(x=10,y=45)
-    entry_17 = tk.Entry(new,textvar=Fecha_produccion)
-    entry_17.pack(padx=5,pady=5,ipadx=5,ipady=5)
-
-    label_1=tk.Label(new,text="Identificacion: ", font=("Agency FB",14)).place(x=10,y=85)
-    entry_1  = tk.Entry(new,textvar=Identificaion)
-    entry_1 .pack(padx=5,pady=5,ipadx=5,ipady=5)
-
-    label_11=tk.Label(new,text="Nombre: ", font=("Agency FB",14)).place(x=10,y=125)
-    entry_11= tk.Entry(new,textvar=Nombres)
-    entry_11.pack(padx=5,pady=5,ipadx=5,ipady=5)
-
-    label_4 = Label(new, text="------------Conteo de mazorcas------------",bg="lawn green",fg="black" )
-    label_4.pack(padx=4,pady=4,ipadx=4,ipady=4)
-
-    label_5=tk.Label(new,text="Total Mazorcas: ", font=("Agency FB",14)).place(x=10,y=200)
-    entry_5= tk.Entry(new,textvar=Mazorcas)
-    entry_5.pack(padx=5,pady=5,ipadx=5,ipady=5)
-
-    label_4 = Label(new, text="------------Verificacion de calidad------------",bg="lawn green",fg="black")
-    label_4.pack(padx=4,pady=4,ipadx=4,ipady=4)    
-
-    label_2=tk.Label(new,text="Mazorcas comerciales: ", font=("Agency FB",14)).place(x=10,y=275)
-    entry_2= tk.Entry(new,textvar=Mazorcascomercial)
-    entry_2.pack(padx=5,pady=5,ipadx=5,ipady=5)
-
-    label_3=tk.Label(new,text="Mazorcas no comerciales: ", font=("Agency FB",14)).place(x=10,y=315)
-    entry_3= tk.Entry(new,textvar=Mazorcasnocomercial,)
-    entry_3.pack(padx=5,pady=5,ipadx=5,ipady=5)
-
-
-    Button(new, text='Guardar',width=10,bg='brown',fg='white',command=validarproduccion).pack(padx=4,pady=4,ipadx=4,ipady=4)
-    Button(new, text='Borrar',width=10,bg='brown',fg='white',command=limpiar).pack(padx=4,pady=4,ipadx=4,ipady=4)
-    boton20 = tk.Button(new,text='cerrar',bg="orange", command=new.destroy)
-    boton20.pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
-
-def validarproduccion():
+def ValidarProduccion():
     cadena5=Fecha_produccion.get()
     patron5="(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-12])\/(19|20)\d\d$"
     cadena6=Identificaion.get()
@@ -272,6 +226,50 @@ def validarproduccion():
     else:
         messagebox.showwarning("Cuidado","Fecha de produccion incorrecta")
 
+def RegistrarProduccion():
+    
+    ventana.deiconify()
+    new = tk.Toplevel()
+    new.geometry("500x500+700+5")
+    new.configure(background='sea green')
+    new.title("Registrar Produccion")
+    esw=tk.Label(new,text="Digite el siguiente formulario" ,bg="lawn green").pack(padx=5,pady=5,ipadx=5,ipady=5)
+
+    Label(new,text="Fecha de produccion: ", font=("Agency FB",14)).place(x=10,y=45)
+    entry_17 = tk.Entry(new,textvar=Fecha_produccion)
+    entry_17.pack(padx=5,pady=5,ipadx=5,ipady=5)
+
+    Label(new,text="Identificacion: ", font=("Agency FB",14)).place(x=10,y=85)
+    entry_1  = tk.Entry(new,textvar=Identificaion)
+    entry_1 .pack(padx=5,pady=5,ipadx=5,ipady=5)
+
+    Label(new,text="Nombre: ", font=("Agency FB",14)).place(x=10,y=125)
+    entry_11= tk.Entry(new,textvar=Nombres)
+    entry_11.pack(padx=5,pady=5,ipadx=5,ipady=5)
+
+    label_4 = Label(new, text="------------Conteo de mazorcas------------",bg="lawn green",fg="black" )
+    label_4.pack(padx=4,pady=4,ipadx=4,ipady=4)
+
+    Label(new,text="Total Mazorcas: ", font=("Agency FB",14)).place(x=10,y=200)
+    entry_5= tk.Entry(new,textvar=Mazorcas)
+    entry_5.pack(padx=5,pady=5,ipadx=5,ipady=5)
+
+    label_4 = Label(new, text="------------Verificacion de calidad------------",bg="lawn green",fg="black")
+    label_4.pack(padx=4,pady=4,ipadx=4,ipady=4)    
+
+    Label(new,text="Mazorcas comerciales: ", font=("Agency FB",14)).place(x=10,y=275)
+    entry_2= tk.Entry(new,textvar=Mazorcascomercial)
+    entry_2.pack(padx=5,pady=5,ipadx=5,ipady=5)
+
+    Label(new,text="Mazorcas no comerciales: ", font=("Agency FB",14)).place(x=10,y=315)
+    entry_3= tk.Entry(new,textvar=Mazorcasnocomercial,)
+    entry_3.pack(padx=5,pady=5,ipadx=5,ipady=5)
+
+
+    Button(new, text='Guardar',width=10,bg='brown',fg='white',command=ValidarProduccion).pack(padx=4,pady=4,ipadx=4,ipady=4)
+    Button(new, text='Borrar',width=10,bg='brown',fg='white',command=limpiar).pack(padx=4,pady=4,ipadx=4,ipady=4)
+    boton20 = tk.Button(new,text='cerrar',bg="orange", command=new.destroy)
+    boton20.pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
 
 
 def database_produc():
@@ -304,18 +302,6 @@ def actualizarusu():
     db.Usuarios.update_one({"identificaion": identificaion,"nombres": nombres, "apellidos": apellidos,
     "edad": edad,"correo": correo,"tipo": tipo,"password": password,"estado": estado})
 
-def consulta():
-    identificaion=Identificaion.get()
-    nombres=Nombres.get()
-    apellidos=Apellidos.get()
-    edad=Edad.get()
-    correo=Correo.get()
-    tipo=Tipo.get()
-    password=Password.get()
-    estado=Estado.get()
-    db.Usuarios.find_one({"identificaion": identificaion})
-    
-    #nombres=Nombres.set(db.Usuarios.find_one({"nombres":nombres}))
 
 def abrirmenu():
     ventana.deiconify()
@@ -342,7 +328,7 @@ def abrirmenuoperario():
     e3.pack(padx=5,pady=5,ipadx=5,ipady=5,fill=tk.X)
     boton9 = tk.Button(win,text="Contar Mazorcas",bg="orange", fg="black", command=conteo)
     boton9.pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
-    boton5 = tk.Button(win,text="Producción",bg="orange", fg="black", command=registrarProduccion)
+    boton5 = tk.Button(win,text="Producción",bg="orange", fg="black", command=RegistrarProduccion)
     boton5.pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
     boton10 = tk.Button(win,text="Verificar Calidad",bg="orange", fg="black", command=calidad)
     boton10.pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
@@ -391,10 +377,10 @@ def conteo():
     new.geometry("500x300+700+20")
     new.configure(background='sea green')
     new.title("Registrar Usuario")
-    e3 = tk.Label(new,text="Busqueda y Conteo", bg="lawn green",fg="black").pack(padx=5,pady=5,ipadx=5,ipady=5,fill=tk.X)
-    boton12 = tk.Button(new,text="Tomar foto",bg="orange", fg="black", command= tomarfoto).pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
-    boton23 = tk.Button(new,text="Busqueda",bg="orange", fg="black", command=abre).pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
-    boton32 = tk.Button(new,text="Verificar",bg="orange", fg="black", command=identidad).pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
+    Label(new,text="Busqueda y Conteo", bg="lawn green",fg="black").pack(padx=5,pady=5,ipadx=5,ipady=5,fill=tk.X)
+    Button(new,text="Tomar foto",bg="orange", fg="black", command= tomarfoto).pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
+    Button(new,text="Busqueda",bg="orange", fg="black", command=abre).pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
+    Button(new,text="Verificar",bg="orange", fg="black", command=identidad).pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
 
     boton2 = tk.Button(new,text='cerrar',bg="orange", command=new.destroy)
     boton2.pack(side=tk.TOP,padx=5,pady=5,ipadx=5,ipady=5)
@@ -454,4 +440,3 @@ Mazorcascomercial=StringVar()
 Mazorcasnocomercial=StringVar() 
 
 ventana.mainloop()
-
